5513e52071f3e31fffd4334cdcc88d5a586949aa
MinhTriet <vhmt.abcxyz@gmail.com>
2026-05-17 15:13:34 +0700

    Unfair Sleeping Barber demonstration

---
+++ SleepingBarber/UnfairSleepingBarber.java
@@ -0,0 +1,75 @@
+package SleepingBarber;
+import java.util.concurrent.locks.*;
+
+public class Test {
+    private static final int MAX_CHAIR = 3;
+    private Thread barberThread;
+    private Thread customerThread;
+    private boolean running = true;
+    private int customerCount = 0;
+    private final Lock lock = new ReentrantLock();
+    private Condition notFullCondition = lock.newCondition();
+    private Condition notEmptyCondition = lock.newCondition();
+
+    public static void main(String[] args) {
+        Test test = new Test();
+        
+        test.barberThread.start();
+        test.customerThread.start();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException ie) {ie.printStackTrace();}
+        
+        test.shutdown();
+    
+    }    
+    public Test(){
+        this.barberThread = new Thread(new Runnable() {
+            public void run(){
+                barberJob();
+            } 
+        });
+        this.customerThread = new Thread(new Runnable (){
+            public void run(){
+                customerVisit();
+            }
+        });
+    }
+    private void barberJob (){
+        while (this.running){
+            lock.lock();
+            try {
+                while (customerCount == 0) notEmptyCondition.await();
+
+                customerCount--;
+                System.out.println("customer shaved. Total customer left: "+customerCount);
+                notFullCondition.signal();
+            } catch (InterruptedException ie) {ie.printStackTrace();}
+            finally {
+                lock.unlock();
+            }
+        }
+    }
+    public void shutdown (){
+        this.running = false;
+        this.barberThread.interrupt();
+        this.customerThread.interrupt();
+        try{
+            this.barberThread.join();
+            this.customerThread.join();
+        }catch (InterruptedException ie) {ie.printStackTrace();}
+    }
+    public void customerVisit(){
+        while (this.running){
+            lock.lock();
+            try{
+                while (customerCount == MAX_CHAIR) notFullCondition.await();
+                
+                customerCount++;
+                notEmptyCondition.signal();
+                System.out.println("1 customer joined the line. Total customer waiting: " +customerCount);
+            } catch(InterruptedException ie){ie.printStackTrace();}
+            finally {lock.unlock();}
+        }
+    }
+}
